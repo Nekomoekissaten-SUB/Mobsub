1d09e9e8c451b6272b36b55a0bdeb481a9296e69
MIRIMIRIM <59959583+MIRIMIRIM@users.noreply.github.com>
2024-01-12 14:21:55 +0800

    clamp AssTime

--- SubtitleParse/AssTypes/AssTime.cs
+++ SubtitleParse/AssTypes/AssTime.cs
@@ -1,9 +1,3 @@
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.Numerics;
-
 namespace Mobsub.AssTypes;
 
 public readonly struct AssTime
@@ -11,10 +5,13 @@ public readonly struct AssTime
     private readonly long _ticks;
     private const int MinMilliSeconds = 0;
     private const int MaxMilliSeconds = int.MaxValue;
+    private const int MinTimeTicks = 0;
+    private const long MaxTimeTicks = (long)int.MaxValue* 10000;
+
     public static AssTime MinValue => new AssTime(MinMilliSeconds);
     public static AssTime MaxValue => new AssTime(MaxMilliSeconds);
 
-    public int Hour => new TimeSpan(_ticks).Hours;
+    public int Hour => (int)new TimeSpan(_ticks).TotalHours;    // Truncate
     public int Minute => new TimeSpan(_ticks).Minutes;
     public int Second => new TimeSpan(_ticks).Seconds;
     public int Millisecond => new TimeSpan(_ticks).Milliseconds;
@@ -22,11 +19,10 @@ public readonly struct AssTime
     public int Nanosecond => new TimeSpan(_ticks).Nanoseconds;
     public long Ticks => _ticks;
 
-    public AssTime(long ticks) => _ticks = ticks;
-    public AssTime(int ms) => _ticks = (long)ms * 10000;
+    public AssTime(long ticks) => _ticks = Math.Clamp(ticks, MinTimeTicks, MaxTimeTicks);
+    public AssTime(int ms) => _ticks = Math.Clamp((long)ms * 10000, MinTimeTicks, MaxTimeTicks);
 
-
-    private AssTime AddTicks(long ticks) => new AssTime((_ticks + TimeSpan.TicksPerDay + (ticks % TimeSpan.TicksPerDay)) % TimeSpan.TicksPerDay);
+    private AssTime AddTicks(long ticks) => new AssTime(_ticks + ticks);
 
     public AssTime Add(TimeSpan value) => AddTicks(value.Ticks);
 
