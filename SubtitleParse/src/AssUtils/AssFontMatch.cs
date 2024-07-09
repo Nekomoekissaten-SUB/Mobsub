5a91fca20744ca0346860c6321d67b5a725106d3
MIRIMIRIM <59959583+MIRIMIRIM@users.noreply.github.com>
2024-07-09 16:48:33 +0800

    SubtitleParse/AssUtils: add AssFontMatch (draft)

---
+++ SubtitleParse/src/AssUtils/AssFontMatch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mobsub.Helper.Font;
+using Mobsub.SubtitleParse.AssTypes;
+
+namespace Mobsub.SubtitleParse.AssUtils;
+
+public class AssFontMatch(IEnumerable<FontFaceInfoBase> fontFaceInfos)
+{
+    private readonly IEnumerable<FontFaceInfoBase> faceInfos = fontFaceInfos;
+
+    public FontFaceInfoBase? GetFontFace(AssFontInfo assFontInfo)
+    {
+        var faceInfosArr = faceInfos.ToArray();
+        var sameFamilyNameFaces = faceInfosArr.Where(face => face.FamilyNameGdi == assFontInfo.Name || face.FamilyNameGdiLocalized == assFontInfo.Name);
+
+        if (!sameFamilyNameFaces.Any()) { return null; }
+
+
+
+
+        return null;
+    }
+}
