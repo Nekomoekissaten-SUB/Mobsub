dcd3d1c76a886d6dc7f0b28a63ba8822baf85a8c
MIRIMIRIM <59959583+MIRIMIRIM@users.noreply.github.com>
2024-12-15 02:11:28 +0800

    hide zero value

---
+++ RainCurtain/ViewModels/Converter/HideZeroConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+
+namespace Mobsub.RainCurtain.ViewModels.Converter;
+
+public class HideZeroConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is int i)
+        {
+            return i == 0 ? string.Empty : i;
+        }
+        
+        return null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return BindingOperations.DoNothing;
+    }
+}
