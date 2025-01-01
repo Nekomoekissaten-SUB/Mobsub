3412ffd93aa68b8c36477fe4554a179c36744b1c
MIRIMIRIM <59959583+MIRIMIRIM@users.noreply.github.com>
2025-01-02 01:05:01 +0800

    SimpleTools: add SubtitlesPublic

---
+++ SimpleTools/Mobsub.SubtitlesPublic/Models/BaseConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Mobsub.SubtitlesPublic.Models;
+
+public class BaseConfig
+{
+    public string PrivateRepoPath { get; set; }
+    public string PublicRepoPath { get; set; }
+    public string TempUploadPath { get; set; }
+    public string ListAssFontsBinaryPath { get; set; }
+}
+
+[JsonSourceGenerationOptions(WriteIndented = true, PropertyNamingPolicy = JsonKnownNamingPolicy.SnakeCaseLower)]
+[JsonSerializable(typeof(BaseConfig))]
+public partial class BaseConfigContext : JsonSerializerContext
+{
+}
