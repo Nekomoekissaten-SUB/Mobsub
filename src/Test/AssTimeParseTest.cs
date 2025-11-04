fef8884f8db9447f1b25e5f07663b4ea62089f68
MIRIMIRIM <59959583+MIRIMIRIM@users.noreply.github.com>
2025-11-05 00:37:48 +0800

    update test

---
+++ src/Test/AssTimeParseTest.cs
@@ -0,0 +1,29 @@
+﻿using Mobsub.SubtitleParse.AssTypes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test;
+
+[TestClass]
+public partial class ParseTest
+{
+    private AssTime _time = new(223748000000);
+    private Mobsub.SubtitleParseNT2.AssTypes.AssTime _time2 = new(223748000000);
+
+    [TestMethod]
+    public void AssTimeParseSpan()
+    {
+        var str = "6:12:54.80";
+        var time = AssTime.ParseFromAss(str);
+        Assert.IsTrue(time.Equals(_time));
+    }
+
+    [TestMethod]
+    public void AssTimeParseSpanUtf8()
+    {
+        var str = "6:12:54.80"u8;
+        var time = Mobsub.SubtitleParseNT2.AssTypes.AssTime.ParseFromAss(str);
+        Assert.IsTrue(time.Equals(_time2));
+    }
+}
